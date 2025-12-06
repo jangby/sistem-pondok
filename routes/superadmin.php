430f168630cd4306171342ea2738d75f3f37904c
robaya <robaya05@gmail.com>
2025-12-06 17:02:42 +0700

    baru

--- routes/superadmin.php
+++ routes/superadmin.php
@@ -40,5 +40,6 @@ Route::middleware(['auth', 'role:super-admin'])
 
         Route::get('/computer-manager', [ComputerManagerController::class, 'index'])
     ->name('computer.index'); // <-- Cukup begini
-        Route::post('/computers/{id}/command', [ComputerController::class, 'sendCommand'])->name('computers.command');
+        Route::post('/computer/command/{id}', [App\Http\Controllers\SuperAdmin\ComputerManagerController::class, 'sendCommand'])
+    ->name('/computer.command');
     });
