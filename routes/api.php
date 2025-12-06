44895125db99e8b267343cddef74a60d2b5638f7
robaya <robaya05@gmail.com>
2025-12-06 17:17:09 +0700

    barru

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Sekolah\SekolahApiController;
 use App\Http\Controllers\Api\ComputerLogController;
+use App\Http\Controllers\SuperAdmin\ComputerManagerController;
+
 
 Route::post('/update-pc-password', [ComputerLogController::class, 'store']);
-Route::post('/check-command', [ComputerController::class, 'checkCommand']);
+Route::post('/check-command', [ComputerManagerController::class, 'checkCommand']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
